e3681e48008015ca6d236872f2643cc563d8d964
Deepansu Verma <111002580+deepansu1620@users.noreply.github.com>
2023-06-22 23:25:54 +0530

    Add files via upload

---
+++ src/utils/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
+import appSlice from "./appSlice";
+
+const store = configureStore({
+    reducer: {
+        cart: cartSlice,
+        app: appSlice
+    }
+});
+
+export default store;
